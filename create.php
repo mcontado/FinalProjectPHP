d1a771f9dc15f2d66ee46e45314c6e6eb34dac69
Tet Contado <delrio.tet@gmail.com>
2018-03-12 23:00:30 -0700

    aligned checkboxes and styles fixed

--- create.php
+++ create.php
@@ -12,22 +12,26 @@ $genres = Movie::select_all_genres();
 
         <form name="wishListForm" action="confirmation.php" onsubmit="return validateForm()" method="post">
 
-            <div class="form-group">
-                <label for="movieTitle">Movie Title:</label>
-                <input type="text" class="form-control" id="movieTitle" placeholder="Enter movie title" name="movieTitle">
+            <div class="form-row">
+                <div class="form-group col-md-4">
+                    <label for="movieTitle">Movie Title:</label>
+                    <input type="text" class="form-control" id="movieTitle" placeholder="Enter movie title" name="movieTitle">
+                </div>
+
+                <div class="form-group col-md-4">
+                    <label for="releaseYear">Release Year:</label>
+                    <input type="text" class="form-control" id="releaseYear" placeholder="Enter release year"
+                           name="releaseYear"
+                           onchange="validateYear()">
+                </div>
+
+                <div class="form-group col-md-4">
+                    <label for="imdbID">IMDB ID: </label>
+                    <input type="text" class="form-control" id="imdbId" placeholder="Enter IMDB ID" name="imdbId" pattern="tt\d{7}" title="IMDB ID should start with lowercase double t followed by 7 digit numbers e.g. tt1234567">
+                </div>
             </div>
 
-            <div class="form-group">
-                <label for="releaseYear">Release Year:</label>
-                <input type="text" class="form-control" id="releaseYear" placeholder="Enter release year"
-                       name="releaseYear"
-                       onchange="validateYear()">
-            </div>
 
-            <div class="form-group">
-                <label for="imdbID">IMDB ID: </label>
-                <input type="text" class="form-control" id="imdbId" placeholder="Enter IMDB ID" name="imdbId" pattern="tt\d{7}" title="IMDB ID should start with lowercase double t followed by 7 digit numbers e.g. tt1234567">
-            </div>
 
             <div class="form-group">
                 <label for="comment">Description:</label>
@@ -36,12 +40,14 @@ $genres = Movie::select_all_genres();
 
             <div class="form-group">
                 <label> Genre: </label>
-                <?php foreach ($genres as $genre) : ?>
-                    <label class="checkbox-inline">
-                        <input type="checkbox" name="genre_list[]" value="<?php echo $genre['genreId']; ?>">
-                            <?php echo $genre['genreName']; ?>
-                    </label>
-                <?php endforeach; ?>
+                <ul class="checkbox-grid">
+                    <?php foreach ($genres as $genre) : ?>
+                        <li><input type="checkbox" name="genre_list[]" value="<?php echo $genre['genreId']; ?>" />
+                            <label><?php echo $genre['genreName']; ?></label>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+                <label> </label>
             </div>
 
             <button type="submit" class="btn btn-info btn-rounded"">Add to Watch List</button>
